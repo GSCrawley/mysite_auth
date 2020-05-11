0d2e43df53253578ce5311c62926f6e3b2d4e868
GSCrawley <gscrawley@gmail.com>
2020-05-11 16:42:17 -0700

    server error 500

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9786_9$up$ur0u#szuq35qqu96@ijkbp5z4(6qh$@2ios1uq+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['takeapollmakeapoll.herokuapp.com', '127.0.0.1']
 
